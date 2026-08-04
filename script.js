b2df350f89c98c4b81eaaa3c78acdc7c1a022a45
yufreddy91-spec <yufreddy91@gmail.com>
2026-08-04 21:48:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 console.log("Henry Watch Loaded Successfully!");
+function loadChat() {
+
+const file=document.getElementById("chatFile").files[0];
+
+if(!file){
+alert("Select WhatsApp chat");
+return;
+}
+
+const reader=new FileReader();
+
+reader.onload=function(e){
+
+const text=e.target.result;
+
+const lines=text.split("\n");
+
+const table=document.getElementById("watchTable");
+
+table.innerHTML="";
+
+lines.forEach(line=>{
+
+if(line.includes("₹")){
+
+const row=`
+<tr>
+<td>Dealer</td>
+<td>${line.substring(0,40)}</td>
+<td>${line.match(/₹[0-9,]+/) || ""}</td>
+</tr>
+`;
+
+table.innerHTML+=row;
+
+}
+
+});
+
+};
+
+reader.readAsText(file);
+
+}
